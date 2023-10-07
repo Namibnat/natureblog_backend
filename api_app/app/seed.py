be3032d4d16d8f63f9036d32fa87361c52333924
Vernon Swanepoel <frantic.naturalist@gmail.com>
2023-10-07 19:07:44 +0100

    Using chatGTP generated dummy data so that I can improve front end.

--- api_app/app/seed.py
+++ api_app/app/seed.py
@@ -1,3 +1,5 @@
+import json
+
 from sqlalchemy.orm import sessionmaker
 from slugify import slugify
 
@@ -8,14 +10,15 @@ db = SessionLocal()
 
 
 def seed_db():
-    # Don't seed the database if there are existing value in the BlogPost database table.
     if db.query(BlogPost).first():
         print("Database already has records. Seeding skipped.")
         return
 
-    for index, post in enumerate(['First', 'Second', 'Third', 'Forth']):
-        text = f"My {post} Blog Post"
-        post = BlogPost(title=text, slug=slugify(text), body=f"{text} blog content")
+    with open('seed_data.json', 'r') as fs:
+        posts = json.load(fs)
+
+    for post in posts:
+        post = BlogPost(title=post['title'], slug=slugify(post['title']), body=post['body'])
         db.add(post)
     db.commit()
     print("Database seeded successfully.")
